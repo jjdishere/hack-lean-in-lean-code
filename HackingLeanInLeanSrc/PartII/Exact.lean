02c996ec7acc1bf48ac61b7fe4d044dd901d10ed
jjdishere <emailboxofjjd@163.com>
2023-10-17 14:57:46 +0800

    Update Exact.lean

--- HackingLeanInLeanSrc/PartII/Exact.lean
+++ HackingLeanInLeanSrc/PartII/Exact.lean
@@ -41,9 +41,23 @@ example {A B C : Type} (f : A -> B -> C) (x : A) (y : B) : C := by
 -- that `exact_anyhow` takes one and more arguments, which syntax
 -- is similar to the one for parse many `p*`
 
--- example {A B C : Type} (f : A -> B -> C) (x : A) (y : B) : C := by
---   apply f
---   exact_anyhow x 42
---     somehow
---       anyhow rfl are we proving
---   exact_anyhow y x x x x y
+syntax (name := exact_anyhow) "exact_anyhow" (ppSpace colGt term:max)* : tactic
+
+@[tactic exact_anyhow]
+def evalExactAnyhow : Tactic := fun stx =>
+  match stx with
+  | `(tactic| exact_anyhow $e $_*) =>
+    closeMainGoalUsing (checkUnassigned := false)
+      fun type => do
+        let mvarCounterSaved := (← getMCtx).mvarCounter
+        let r ← elabTermEnsuringType e type
+        logUnassignedAndAbort (← filterOldMVars (← getMVars r) mvarCounterSaved)
+        return r
+  | _ => throwUnsupportedSyntax
+
+example {A B C : Type} (f : A -> B -> C) (x : A) (y : B) : C := by
+   apply f
+   exact_anyhow x 42
+     somehow
+       anyhow rfl are we proving
+   exact_anyhow y x x x x y
